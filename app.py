6c5174ac46e1ebff8247a42c04457476c6975c82
Renan Passos <68907028+renanvpassos@users.noreply.github.com>
2026-05-27 10:46:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -462,7 +462,7 @@ elif opcao == "LOG":
                     html_log = (
                         f'<div class="card-log" style="position: relative; margin-bottom: 10px;">'
                         f'<span style="float: right; color: gray; font-size: 0.85em; text-align: right; line-height: 1.2;">'
-                        f'📅 {evento["data_str"]}<br>💻 {hora_sistema}'
+                        f'📅 {evento["data_str"]}<br> {hora_sistema}'
                         f'</span>'
                         f'⏱️ <b>{evento["hora_str"]}</b> - <b>{evento["nome"]}</b> {evento["acao"]}'
                         f'{html_justificativa}'  # A justificativa entra exatamente aqui, abaixo da linha principal
